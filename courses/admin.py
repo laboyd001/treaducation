3454b310a2730c611770ee5d963a558b1301e434
Lesley Boyd <lesley.a.boyd1@gmail.com>
2019-03-06 10:25:20 -0600

    added a header with the app name to the landing page

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Subject, Course, Module
 
-# Register your models here.
+
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    '''This adds the subjects to the django admin portal
+    '''
+
+    list_display = ['title', 'slug']
+    prepopulated_fields = {'slug': ('title',)}
+
+
+class ModuleInline(admin.StackedInline):
+    model = Module
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    '''This adds the courses to the django admin portal
+    '''
+
+    list_display = ['title', 'subject', 'created']
+    list_filter = ['created', 'subject']
+    search_fields = ['title', 'overview']
+    prepopulated_fields = {'slug': ('title',)}
+    inlines = [ModuleInline]
