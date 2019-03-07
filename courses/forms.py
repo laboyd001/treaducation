78d0301a165e362e9d66c20609b75b3edabca8ff
Lesley Boyd <lesley.a.boyd1@gmail.com>
2019-03-07 16:41:40 -0600

    added forms file for later operations

--- courses/forms.py
+++ courses/forms.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.models import User
 from django import forms
 
+from .models import Course, Module
+
+
 
 
 class UserForm(forms.ModelForm):
@@ -9,3 +12,27 @@ class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password', 'first_name', 'last_name',)
+
+
+class Course_Form(forms.ModelForm):
+    class Meta:
+        model = Course
+        fields = ['subject', 'title', 'overview']
+        labels = {
+            'subject': 'Subject',
+            'title': 'Course Title',
+            'overview': 'Course Overview',
+        }
+       
+
+class Module_Form(forms.ModelForm):
+    class Meta:
+        model = Module
+        fields = ['course','title', 'description', 'text', 'image']
+        labels = {
+            'course': 'Course',
+            'title': 'Title',
+            'description': 'Description',
+            'text': 'Module Text',
+            'image': 'Image Content'  
+        }
